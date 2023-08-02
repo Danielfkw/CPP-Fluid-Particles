19a511baa9e75fd592b9633d5c9b279a60f493aa
fkwfeng <fkwfeng@gmail.com>
2023-08-02 19:26:46 +0800

    Update BaseSolver.h
    Test 😂😂😂😂

--- src/BaseSolver.h
+++ src/BaseSolver.h
@@ -15,6 +15,7 @@
 // You should have received a copy of the GNU General Public License
 // along with CPP-Fluid-Particles.  If not, see <http://www.gnu.org/licenses/>.
 
+// This is test
 #pragma once
 
 class BaseSolver {
